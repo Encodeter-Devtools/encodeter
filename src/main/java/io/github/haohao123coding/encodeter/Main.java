3ba9f843790140049cbc4106f8273db9f0caf0e4
Haohao123coding <18217288892@163.com>
2025-11-16 19:45:55 +0800

    code: delete main() in Main.java

--- src/main/java/io/github/haohao123coding/encodeter/Main.java
+++ src/main/java/io/github/haohao123coding/encodeter/Main.java
@@ -21,8 +21,4 @@ public class Main extends Application{
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    public static void main(String[] args){
-        launch(args);
-    }
 }
