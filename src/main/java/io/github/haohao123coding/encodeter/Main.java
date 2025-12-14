ea9a701be1854ae1492ede9b8500d16dd5f35f22
Haohao123coding <18217288892@163.com>
2025-12-14 16:25:13 +0800

    bug: fix main() function in Main.java

--- src/main/java/io/github/haohao123coding/encodeter/Main.java
+++ src/main/java/io/github/haohao123coding/encodeter/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage){
